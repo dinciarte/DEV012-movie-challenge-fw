0451e1e0cc8ea0f0c1e8f8f9097296a7b1899757
Diveana Inciarte <dinciarte25@gmail.com>
2024-01-09 20:20:15 -0300

    Se logra renderizar las películas y se empieza a trabajar en los estilos

--- movie-challenge/src/app/app-routing.module.ts
+++ movie-challenge/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ErrorComponent } from './views/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "error", component: ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
